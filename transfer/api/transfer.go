38ff2a1f45e3c786993a5eb51e13d5f44355256e
endkarn <end_karn@hotmail.com>
2019-08-14 16:39:08 +0700

    [Added]: UnitTest ของการ Decode ค่าที่มาจาก RequestBody

--- transfer/api/transfer.go
+++ transfer/api/transfer.go
@@ -1,2 +1,7 @@
 package api
 
+import "transfer/model"
+
+func DecodeTransaction(requestBody string) (model.TransactionModel,error) {
+	return model.TransactionModel{},nil
+}
--- transfer/api/transfer_test.go
+++ transfer/api/transfer_test.go
@@ -1,1 +1,33 @@
 package api
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"transfer/model"
+)
+
+func Test_DecodeTransaction_By_Valid_RequestBody_Should_Be_TransactionData(t *testing.T){
+	expected := model.TransactionModel{
+		SourceAccount:      model.AccountModel{
+			Number:"0563219470",
+		},
+		DestinationAccount: model.AccountModel{
+			Number:"0571810042",
+		},
+		AmountTransfer:     0,
+	}
+	requestBody := `{"source_account":"0563219470","destination_account":"0571810042","amount_transfer":9000.00}`
+
+	actual , _:= DecodeTransaction(requestBody)
+
+	assert.Equal(t,expected,actual)
+}
+
+func Test_DecodeTransaction_By_UnValid_RequestBody_Should_Be_Empty_Object(t *testing.T){
+	expected := model.TransactionModel{}
+	requestBody := `{"source_account":"0563219470","destination_account":"0571810042","amount_transfer":9000.00}`
+
+	actual , _ := DecodeTransaction(requestBody)
+
+	assert.Equal(t,expected,actual)
+}
